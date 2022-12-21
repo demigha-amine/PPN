44515bbe133e25419d17ae6447f6a828a60bed11
demigha amine <hamza.demigha@ens.uvsq.fr>
2022-12-21 21:54:08 +0100

    MNIST READER
    Un exemple de lecture des images avec label associes

---
+++ mnist_reader/example_use.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include "mnist_reader.h"
+
+int main(void) {
+	FILE* imageFile = fopen("mnist/train-images-idx3-ubyte", "r");
+	FILE* labelFile = fopen("mnist/train-labels-idx1-ubyte", "r");
+
+	// Read 10 images from the 50 image
+	uint8_t* images = readMnistImages(imageFile, 50, 15);
+	uint8_t* labels = readMnistLabels(labelFile, 50, 15);
+
+	fclose(imageFile);
+	fclose(labelFile);
+
+	for(int i=0; i<15; i++) {
+		printf("i=%d\n", i);
+		printAsciiDigit(images + i*784*sizeof(uint8_t));
+		printf("Label=%d\n", *(labels+i));
+		printf("********************************\n");
+	}
+
+	free(images);
+	free(labels);
+
+	return 0;
+}
