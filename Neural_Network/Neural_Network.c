aa70b32bb61fcb15988814ae6b59dc57ae9374df
Keddad Youcef <youcef.keddad@ens.uvsq.fr>
2022-12-30 16:38:56 +0100

    Update les fonctions create_network() et train_network()

--- Neural_Network/Neural_Network.c
+++ Neural_Network/Neural_Network.c
@@ -15,13 +15,9 @@ NeuralNetwork* create_network(int input, int hidden, int output, double lr) {
 	
 	network->hidden_weights = create_mat(hidden, input);
 	network->output_weights = create_mat(output, hidden);
-	network->hidden_bias = create_mat(hidden,1);
-	network->output_bias = create_mat(output,1);
 
 	Rand_Matrice(network->hidden_weights);
 	Rand_Matrice(network->output_weights);
-	Rand_Matrice(network->hidden_bias);   
-	Rand_Matrice(network->output_bias);
 
 	return network;
 }
@@ -33,130 +29,79 @@ void train_network(NeuralNetwork* net, Matrice* input_data, Matrice* output_data
 	// 1ere Etape (Hidden Layer)
 	
 	Matrice* hidden_inputs	= dotprod(net->hidden_weights, input_data);
-	Matrice* z_hidden = add(hidden_inputs, net->hidden_bias);
-	Matrice* hidden_outputs = apply(sigmoid, z_hidden);
+	Matrice* hidden_outputs = apply(sigmoid, hidden_inputs);
 	
 
 	// 2eme Etape (Output Layer)
 	Matrice* final_inputs = dotprod(net->output_weights, hidden_outputs);
-	Matrice* z_output = add(final_inputs, net->output_bias);
-	Matrice* final_outputs = apply(sigmoid, z_output);
+	Matrice* final_outputs = apply(sigmoid, final_inputs);
 
+	
 
 
 	// // Matrices des erreurs
 	Matrice* output_errors = sub(output_data, final_outputs);
-
-
-	Matrice* transposed_output_weights = transpose(net->output_weights);
-	Matrice* hidden_errors = dotprod(transposed_output_weights, output_errors);
+	Matrice* hidden_errors = dotprod(transpose(net->output_weights), output_errors);
 	
 
 
 
 	// Backpropagation Processus
 
-	// Adjusting weights and biases for the output layer	
+	// Adjusting weights for the output layer	
 	// weights : dC/dw = (dz/dw)*(da/dz)/(dC/da)
-	// biases: dC/db = (dz/db)*(da/dz)/(dC/da)
 
 
 
-	Matrice* dsigmoid_z_mat = dSigmoid(z_output);
-	Matrice* multiplied_mat = mult(output_errors, dsigmoid_z_mat);
-
-
-	// Adjusting biases
-	Matrice* scaled_bias_mat = scale(net->LR, multiplied_mat);
-	Matrice* fixed_bias_mat = add(net->output_bias, scaled_bias_mat);
-	
-
-
-
-	// Adjusting weights
-	
+	Matrice* sigmoid_primed_mat = dSigmoid(final_outputs);
+	Matrice* multiplied_mat = mult(output_errors, sigmoid_primed_mat);
 	Matrice* transposed_mat = transpose(hidden_outputs);
 	Matrice* dot_mat = dotprod(multiplied_mat, transposed_mat);
 	Matrice* scaled_mat = scale(net->LR, dot_mat);
-	Matrice* fixed_weights_mat = add(net->output_weights, scaled_mat);
+	Matrice* added_mat = add(net->output_weights, scaled_mat);
 
 
-	free_mat(net->output_bias); 				// free the old output bias matrix before replacing
-	net->output_bias = fixed_bias_mat;			// Remplacement par la matrice fixe
-	
-	free_mat(net->output_weights); 				// free the old output weights matrix before replacing
-	net->output_weights = fixed_weights_mat;	// Remplacement par la matrice fixe
+	free_mat(net->output_weights);			 // free the old output weights matrix before replacing
+	net->output_weights = added_mat;		// Remplacement par la matrice correcte
 
-	
-	// // free_mat the matrices pour les utiliser au prochaine etape
-	free_mat(dsigmoid_z_mat);
+	free_mat(sigmoid_primed_mat);
 	free_mat(multiplied_mat);
 	free_mat(transposed_mat);
 	free_mat(dot_mat);
 	free_mat(scaled_mat);
-	free_mat(scaled_bias_mat);
 	
 	
 
 
-	// // Adjusting weights and biases  for the hidden layer	
+	// // Adjusting weights for the hidden layer	
 	// // weights : dC/dw = (dz/dw)*(da/dz)/(dC/da)
-	// // biases: dC/db = (dz/db)*(da/dz)/(dC/da)
 
 	
 	
-	dsigmoid_z_mat = dSigmoid(z_hidden);
-	// printf("dsigmoid_z_mat\n");
-	// affiche_mat(dsigmoid_z_mat);
-	multiplied_mat = mult(hidden_errors, dsigmoid_z_mat);
-	// printf("Multiplied Mat for the hidden layer\n");
-	// affiche_mat(multiplied_mat);
-
-
-	// Adjusting biases
-	scaled_bias_mat = scale(net->LR, multiplied_mat);
-	// printf("scaled_bias_mat\n");
-	// affiche_mat(scaled_bias_mat);
-	Matrice* fixed_hidden_bias_mat = add(net->hidden_bias, scaled_bias_mat);
-
-	// Adjusting weights
+	sigmoid_primed_mat = dSigmoid(hidden_outputs);
+	multiplied_mat = mult(hidden_errors, sigmoid_primed_mat);
 	transposed_mat = transpose(input_data);
 	dot_mat = dotprod(multiplied_mat, transposed_mat);
 	scaled_mat = scale(net->LR, dot_mat);
-	Matrice* fixed_hidden_weights_mat = add(net->hidden_weights, scaled_mat);
+	added_mat = add(net->hidden_weights, scaled_mat);
 
+	free_mat(net->hidden_weights); 				// free the old hidden weights matrix before replacing
+	net->hidden_weights = added_mat; 			// Remplacement par la matrice correcte
 
-
-	free_mat(net->hidden_bias); 				// free_mat the old hidden bias matrix before replacing
-	net->hidden_bias = fixed_hidden_bias_mat;			// Remplacement par la matrice fixe
-	
-	free_mat(net->hidden_weights); 				// free_mat the old hidden weights matrix before replacing
-	net->hidden_weights = fixed_hidden_weights_mat;	// Remplacement par la matrice fixe
-
-	
-	
-	
-	// free_mat the matrices
-	free_mat(dsigmoid_z_mat);
+	free_mat(sigmoid_primed_mat);
 	free_mat(multiplied_mat);
 	free_mat(transposed_mat);
 	free_mat(dot_mat);
 	free_mat(scaled_mat);
-	free_mat(scaled_bias_mat);
 
-
-
+	// Free matrices
 	free_mat(hidden_inputs);
-	free_mat(z_hidden);
 	free_mat(hidden_outputs);
-	free_mat(z_output);
 	free_mat(final_inputs);
 	free_mat(final_outputs);
 	free_mat(output_errors);
 	free_mat(hidden_errors);
 }
-
-
 
 
 
